2d990f56bca6eadd3668a838734fdee39bc1014e
RodutLikesArk <133697084+RodutLikesArk@users.noreply.github.com>
2023-09-18 19:38:51 +0300

    Add files via upload

---
+++ Output.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+      
+   
+    let data = "Learning how to write in a file."
+      
+    
+    fs.writeFile('Output.txt', data, (err) => {
+          
+        
+        if (err) throw err;
+    })
